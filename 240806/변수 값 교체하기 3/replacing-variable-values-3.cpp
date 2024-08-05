b92c4666ca5c7097ae2b074958a181505b79021c
hozzang03 <168700717+hozzang03@users.noreply.github.com>
2024-08-06 00:31:37 +0900

    Title: 변수 값 교체하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240806/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a =3;
+    int b= 5;
+
+    int tmp;
+
+    tmp = a;
+    a=b;
+    b=tmp; 
+
+    cout << a << '\n' <<b;
+
+    return 0;
+}
