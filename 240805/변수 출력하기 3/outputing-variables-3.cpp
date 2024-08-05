d5df2d39e504a78fad311b5555dbeddf3e9f4c5b
hozzang03 <168700717+hozzang03@users.noreply.github.com>
2024-08-05 19:23:11 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240805/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a,b;
+    char c;
+
+    a= 1;
+    b = 2;
+    c = 'C';
+
+    cout << a << "->" << b << "->" << c;
+    return 0;
+}
