2d9cf4528e9a0bd2307ab9f058dc1f638eefbd87
hozzang03 <168700717+hozzang03@users.noreply.github.com>
2024-08-06 20:08:28 +0900

    Title: 배열 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240806/배열 채우기/filling-array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int arr[10];
+    int count = 0;
+
+    for(int i = 0;  i < 10; i++) {
+        cin >> arr[i];
+        if(arr[i] == 0) {
+            break;
+        }
+        count ++;
+    }
+
+    for(int i = count - 1;  i >= 0; i--) {
+        cout << arr[i]<<' ';
+    }
+
+
+    return 0;
+}
