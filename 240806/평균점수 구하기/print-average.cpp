31a3028d4f19cfaa011ab192a6b56704cbac46b4
hozzang03 <168700717+hozzang03@users.noreply.github.com>
2024-08-06 19:43:23 +0900

    Title: 평균점수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240806/평균점수 구하기/print-average.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    double arr[8];
+    double sum;
+
+    for(int i = 0; i < 8; i++) {
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < 8; i++) {
+        sum += arr[i];
+    }
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << sum / 8; 
+
+    return 0;
+}
