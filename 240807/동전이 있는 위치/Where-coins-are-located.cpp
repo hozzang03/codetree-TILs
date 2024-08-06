2012f6a5169bae03ffcb0ea41ae0f707b9281bd7
hozzang03 <168700717+hozzang03@users.noreply.github.com>
2024-08-07 03:56:16 +0900

    Title: 동전이 있는 위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240807/동전이 있는 위치/Where-coins-are-located.cpp
+++ 240807/동전이 있는 위치/Where-coins-are-located.cpp
@@ -11,8 +11,8 @@ int main() {
     for(int i = 0; i < m; i++){
         int r, c;
         cin >> r >> c;
-        arr[r-1][c-1] = 1;  // -1 해야함 >> 입력된 좌표는 1부터 시작하는 값으로 입력되므로,
-        // 배열 인덱스로 사용할 때는 r - 1과 c - 1로 변환해야 함
+        arr[r-1][c-1] = 1;
+    }
 
     for(int i = 0 ; i < n; i ++) {
         for(int j = 0; j < n; j++) {
