f7ac2171935e2c0a1f5d802a98a8b7d920c1c32c
hozzang03 <168700717+hozzang03@users.noreply.github.com>
2024-10-13 19:03:45 +0900

    Title: 최대공약수 구하기, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 241013/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 241013/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -4,12 +4,7 @@ n, m = int(n), int(m)
 mx = n
 ans = 0
 def max_divi(n, m) :
-    if n > m :
-        mx = n
-    else :
-        mx = m
-    
-    for i in range(mx, -1, -1) :
+    for i in range(min(n,m), -1, -1) :
         if m%i == 0 and n%i == 0 :
             print(i)
             break
