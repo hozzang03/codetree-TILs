c3c8ebfca60338fb7fe81eb123b615aac2606755
hozzang03 <168700717+hozzang03@users.noreply.github.com>
2024-10-14 20:04:36 +0900

    Title: 2021년 날짜의 유무, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 241014/2021년 날짜의 유무/with-or-without-2021.py
+++ 241014/2021년 날짜의 유무/with-or-without-2021.py
@@ -2,10 +2,10 @@ M, D = tuple(map(int, input().split()))
 
 def month(m, d) :
     if 1 <= m <= 12 :
-        if m % 2 == 0 and m != 2 :
+        if m == 4 or m == 6 or m == 9 or m == 11 :
             if 1 <= d <= 30 :
                 return True
-        elif m%2 == 1 :
+        elif m == 1 or m == 3 or m==5 or m==7 or m == 8 or m == 10 or m == 12 :
             if 1 <= d <= 31 :
                 return True
         elif m == 2 :
@@ -13,7 +13,7 @@ def month(m, d) :
                 return True
     
     return False
-    
+
 if month(M, D) :
     print("Yes")
 else :
