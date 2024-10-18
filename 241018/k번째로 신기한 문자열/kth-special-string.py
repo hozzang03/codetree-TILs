c623e0f283fbf1fdde159ec98aae3298d6a7dcd3
hozzang03 <168700717+hozzang03@users.noreply.github.com>
2024-10-18 18:17:55 +0900

    Title: k번째로 신기한 문자열, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

--- 241018/k번째로 신기한 문자열/kth-special-string.py
+++ 241018/k번째로 신기한 문자열/kth-special-string.py
@@ -12,6 +12,7 @@ def check(n) :
     for i in range(len(T)) :
         if st[n][i] != T[i] :
             return False
+    return True
 
 
 st2 = []
@@ -20,6 +21,7 @@ for i in range(n) :
     if check(i) :
         st2.append(st[i])
 
-st.sort()
+st2.sort()
 
-print(st[k])
+#print(st2)
+print(st2[k-1])
