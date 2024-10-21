ea1f7115e6b224bff6b1fd59ca2b831be72a27e4
hozzang03 <168700717+hozzang03@users.noreply.github.com>
2024-10-21 16:43:35 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 115ms, Memory: 28MB, Status: Passed - CodeTree

--- 241021/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
+++ 241021/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
@@ -1,19 +1,21 @@
+class num :
+    def __init__(self, number, index) :
+        self.n = number
+        self.idx = index
+
 n = int(input())
 ar = list(map(int, input().split())) 
-ar2 = sorted(ar)
-ar3 = []
-# ar  = 3 1 6 2 7 30 1
-# ar2 = 0 0 0 0 0  0 0
-# ar3 = 1 1 2 3 6 7 30
-cnt = 1
-for elem in ar :
-    ans = ar2.index(elem)+1
-    if ans not in ar3 :
-        ar3.append(ans)
-        cnt = 1
-    else :
-        ar3.append(ans+cnt)
-        cnt += 1
 
-for elem in ar3 :
+ans = [0]*n
+number = [
+    num(elem, idx)
+    for idx, elem in enumerate(ar)
+]
+
+number.sort(key = lambda x : (x.n, x.idx))
+
+for idx, elem in enumerate(number) :
+    ans[elem.idx] = idx + 1
+
+for elem in ans :
     print(elem, end = ' ')
