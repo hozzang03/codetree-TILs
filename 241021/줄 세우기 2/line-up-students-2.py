d80ae406ceb54d23481736771d046acbdebe996f
hozzang03 <168700717+hozzang03@users.noreply.github.com>
2024-10-21 16:50:43 +0900

    Title: 줄 세우기 2, Time: 156ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241021/줄 세우기 2/line-up-students-2.py
@@ -0,0 +1,17 @@
+class info :
+    def __init__(self, height, weight, number) :
+        self.h = height
+        self.w = weight
+        self.n = number
+
+n = int(input())
+
+st = []
+for i in range(1, n+1) :
+    h,w = tuple(map(int, input().split()))
+    st.append(info(h, w, i))
+
+st.sort(key = lambda x : (x.h, -x.w))
+
+for s in st :
+    print(s.h, s.w, s.n)
