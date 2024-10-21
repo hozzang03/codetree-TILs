36a2fa47274bd411d9426318605f938d3ba36bc3
hozzang03 <168700717+hozzang03@users.noreply.github.com>
2024-10-21 16:34:35 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241021/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
+++ 241021/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
@@ -5,13 +5,14 @@ ar3 = []
 # ar  = 3 1 6 2 7 30 1
 # ar2 = 0 0 0 0 0  0 0
 # ar3 = 1 1 2 3 6 7 30
-
+cnt = 1
 for elem in ar :
     ans = ar2.index(elem)+1
     if ans not in ar3 :
         ar3.append(ans)
     else :
-        ar3.append(ans+1)
+        ar3.append(ans+cnt)
+        cnt += 1
 
 for elem in ar3 :
     print(elem, end = ' ')
