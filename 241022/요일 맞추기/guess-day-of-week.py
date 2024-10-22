0a121eca95930619d17d38bbafbe61366c19224f
hozzang03 <168700717+hozzang03@users.noreply.github.com>
2024-10-22 15:29:26 +0900

    Title: 요일 맞추기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 241022/요일 맞추기/guess-day-of-week.py
+++ 241022/요일 맞추기/guess-day-of-week.py
@@ -1,7 +1,7 @@
 m1, d1, m2, d2 = tuple(map(int, input().split()))
 ans = 0 
 ar = [31, 28 ,31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 while 1 :
     if m1 == m2 and d1 == d2 :
@@ -13,15 +13,15 @@ while 1 :
 
     elif m1 > m2 :
         d1 -= 1
-        ans += 1
+        ans -= 1
 
     elif m1 == m2 and d1 > d2 :
         d1 -= 1
-        ans + 1
+        ans -= 1
     else :
         d1 += 1
         ans += 1
-        
+
     if d1 > ar[m1-1] :
         m1 += 1
         d1 = 1
@@ -29,5 +29,5 @@ while 1 :
     elif d1 == 0 :
         m1 -= 1
         d1 = ar[m1-1]
-
-print(week[ans//7])
+# print(ans)
+print(week[abs(ans%7)])
