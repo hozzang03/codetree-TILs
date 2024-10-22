466e05b8f7492190012e118d456f15b076e8f26b
hozzang03 <168700717+hozzang03@users.noreply.github.com>
2024-10-22 14:34:11 +0900

    Title: Date to Date, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 241022/Date to Date/date-to-date.py
+++ 241022/Date to Date/date-to-date.py
@@ -1,20 +1,17 @@
 m1, d1, m2, d2 = tuple(map(int, input().split()))
 
 ar = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-ans = 0
+ans = 1
 
 while True :
     if m1 == m2 and d1 == d2 :
         break
 
-    d1 += 1
     ans += 1
+    d1 += 1
 
     if d1 > ar[m1-1] :
         m1 += 1
-        d1 = 0
-    
-if ans != 0 :
-    print(ans)
-else :
-    print("1")
+        d1 = 1
+
+print(ans)
