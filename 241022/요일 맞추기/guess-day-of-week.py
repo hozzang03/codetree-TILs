fdf12e8461fb19068927e060ab9288f28f614405
hozzang03 <168700717+hozzang03@users.noreply.github.com>
2024-10-22 15:34:54 +0900

    Title: 요일 맞추기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241022/요일 맞추기/guess-day-of-week.py
+++ 241022/요일 맞추기/guess-day-of-week.py
@@ -3,31 +3,16 @@ ans = 0
 ar = [31, 28 ,31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
-while 1 :
-    if m1 == m2 and d1 == d2 :
-        break
+def total_day(m, d) :
+    cnt = 0
 
-    if m1 < m2 :
-        d1 += 1
-        ans += 1
+    for i in range(1, m) :
+        cnt += ar[i-1]
+    
+    cnt += d
 
-    elif m1 > m2 :
-        d1 -= 1
-        ans -= 1
+    return cnt
 
-    elif m1 == m2 and d1 > d2 :
-        d1 -= 1
-        ans -= 1
-    else :
-        d1 += 1
-        ans += 1
+ans = total_day(m1, d1) - total_day(m1, d2)
 
-    if d1 > ar[m1-1] :
-        m1 += 1
-        d1 = 1
-
-    elif d1 == 0 :
-        m1 -= 1
-        d1 = ar[m1-1]
-# print(ans)
 print(week[abs(ans%7)])
