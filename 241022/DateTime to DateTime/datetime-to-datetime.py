6269ac3b93f4bf1410b0016f7291c3f6087ef43e
hozzang03 <168700717+hozzang03@users.noreply.github.com>
2024-10-22 14:48:58 +0900

    Title: DateTime to DateTime, Time: 5068ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

---
+++ 241022/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,26 @@
+d, h, m = tuple(map(int, input().split()))
+ans = 0 
+date = str(d) + str(h) + str(m)
+a, b, c = 11, 11, 11
+
+while True :
+    # 날짜가 11일 11시 11분 아래일때
+    if date < "111111" :
+        print("-1")
+        break
+
+    if a == d and b == h and c == m :
+        break
+
+    ans += 1
+    c += 1
+
+    if c == 60 :
+        b += 1
+        c = 0
+    
+    if b == 24 :
+        a += 1
+        b = 0
+
+print(ans)
