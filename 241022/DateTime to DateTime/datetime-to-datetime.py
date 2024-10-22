51191847384eb13f524066b3981481982ec03562
hozzang03 <168700717+hozzang03@users.noreply.github.com>
2024-10-22 15:02:10 +0900

    Title: DateTime to DateTime, Time: 111ms, Memory: 25MB, Status: Passed - CodeTree

--- 241022/DateTime to DateTime/datetime-to-datetime.py
+++ 241022/DateTime to DateTime/datetime-to-datetime.py
@@ -1,28 +1,25 @@
 d, h, m = tuple(map(int, input().split()))
 ans = 0 
-date = str(d) + str(h) + str(m)
-a, b, c = 11, 11, 11
+a, b, c = 11, 11, 11  # 기준 날짜 11일 11시 11분
 
-while True :
-    # 날짜가 11일 11시 11분 아래일때
-    if date < "111111" :
-        print("-1")
-        ans = -1
-        break
+# 입력된 날짜가 기준 날짜보다 이전일 경우
+if d < a or (d == a and h < b) or (d == a and h == b and m < c):
+    print("-1")
+else:
+    # 날짜가 기준 날짜보다 크거나 같을 때 차이 계산
+    while True:
+        if a == d and b == h and c == m:
+            break
 
-    if a == d and b == h and c == m :
-        break
+        ans += 1
+        c += 1
 
-    ans += 1
-    c += 1
+        if c == 60:
+            b += 1
+            c = 0
 
-    if c == 60 :
-        b += 1
-        c = 0
-    
-    if b == 24 :
-        a += 1
-        b = 0
+        if b == 24:
+            a += 1
+            b = 0
 
-if ans != -1 :
     print(ans)
